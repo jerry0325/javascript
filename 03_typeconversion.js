b665b55936bec578248ccb5917317931773f1b4b
JERIN PONNACHAN <jerin.abraham7@gmail.com>
2023-09-25 23:13:29 +0530

    conversion of data types

---
+++ 03_typeconversion.js
@@ -0,0 +1,26 @@
+let score="10";
+
+
+console.log(typeof score);
+
+
+let tonumber= Number(score);
+console.log(typeof tonumber)
+
+// "33" == 33
+
+// "jerry"= you eill get nan - not anumber as output 
+
+// true =1 false =0 
+
+let isLoggedIn =1 ;
+
+console.log(isLoggedIn);
+
+let cboolean = Boolean(isLoggedIn);
+
+console.log(cboolean);
+
+
+// "jerry" => true 
+// " " => false
